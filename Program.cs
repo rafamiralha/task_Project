9b59b8a7b222ed4a4aef930f100f441bf11e8779
Rafael Miralha <rafamiralha1@gmail.com>
2025-03-21 02:16:45 -0300

    feat: finalizado CRUD via Terminal, testes.

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ class Program
 
         while (true)
         {
-            Console.WriteLine("1-Criar | 2-Listar | 3-Excluir | 4-Sair");
+            Console.WriteLine("1-Criar | 2-Listar | 3-Editar | 4-Excluir | 5-Sair");
             string opcao = Console.ReadLine();
 
             switch (opcao)
@@ -40,15 +40,44 @@ class Program
                     break;
 
                 case "3":
+                    {
+                        Console.Write("ID do produto para editar: ");
+                        if (!int.TryParse(Console.ReadLine(), out int Id))
+                        {
+                            Console.WriteLine("ID inválido.");
+                            return;
+                        }
+
+                        var produto = Tasks.Find(p => p.Id == Id);
+                        if (produto == null)
+                        {
+                            Console.WriteLine("Produto não encontrado!");
+                            return;
+                        }
+
+                        Console.Write($"Novo titulo ({produto.title_task}): ");
+                        string? newTitle_task = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(newTitle_task))
+                            produto.title_task = newTitle_task;
+
+                        Console.Write($"Nova Descricao ({produto.description_task}): ");
+                        string? newDescription_task = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(newDescription_task))
+                            produto.description_task = newDescription_task;
+
+                        JsonDb.Salvar(Tasks);
+                        Console.WriteLine("Task atualizada com sucesso!");
+                    }
+                    break;
+
+                case "4":
                     Console.Write("ID da Task para excluir: ");
                     int id = Convert.ToInt32(Console.ReadLine());
                     Tasks.RemoveAll(p => p.Id == id);
                     JsonDb.Salvar(Tasks);
                     break;
-
-                case "4":
+                case "5":
                     return;
-
                 default:
                     Console.WriteLine("Opção inválida!");
                     break;
